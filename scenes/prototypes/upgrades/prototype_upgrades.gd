dc98961564671b7d00cc4dc833a0f4670e1231f9
ThePoetics <87587068+ThePoetics@users.noreply.github.com>
2024-06-27 17:18:55 -0700

    Cleaned file structure, removed more prototypes (Ep 15.1)

--- scenes/prototypes/upgrades/prototype_upgrades.gd
+++
@@ -1,9 +0,0 @@
-extends View
-class_name PrototypeUpgrades
-## Prototype view for displaying Upgrades
-
-## Connect to the parent _ready function and then set visibility
-func _ready() -> void:
-	super()
-	# Defaults to hidden
-	visible = false
